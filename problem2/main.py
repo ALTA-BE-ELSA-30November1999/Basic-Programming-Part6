126e9dc8ec540ba5385da086fa7caf79c32ea5ba
Elsa <elsafirmaniar5@gmail.com>
2023-09-27 23:51:35 +0700

    Solve Problem2

--- problem2/main.py
+++ problem2/main.py
@@ -1,5 +1,18 @@
+Abjad = ['a', 'b', 'c', 'd', 'e', 'f', 'g',
+         'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+         'o', 'p', 'q', 'r', 's', 't', 'u',
+         'v', 'w', 'x', 'y', 'z']
+
 def caesar(offset, input_str):
-    return ''
+    print("\'", end="")
+    str = ''
+    for a in range (len(input_str)) :
+        Hasil_akhir = Abjad.index(input_str[a]) + offset
+        str += Abjad [ Hasil_akhir % 26]
+    for Hasil in str :
+        print(Hasil, end='')
+    print("\'", end="")
+    return str
 
 if __name__ == '__main__':
     print(caesar(3, "abc")) # def
