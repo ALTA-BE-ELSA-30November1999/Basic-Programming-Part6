01636f4b8b0da787a50ffa7d2fd502d0682065f5
Elsa <elsafirmaniar5@gmail.com>
2023-09-27 23:19:29 +0700

    Solve Problem5

--- problem5/main.py
+++ problem5/main.py
@@ -1,5 +1,18 @@
 def remove_duplicates(array):
-    return 0
+    # count = 0
+    Hasil = []
+    for i in array :
+        H = array.count(i)
+        # print (H, end="")
+        if H >= 2 :
+            if i not in Hasil :
+                Hasil.append(i)
+                # print (i, end="")
+        else : 
+            Hasil.append(i)
+            # print(i, end="")
+    Jumlah = len(Hasil)
+    return Jumlah
 
 if __name__ == '__main__':
     print(remove_duplicates([2, 3, 3, 3, 6, 9, 9])) # 4
